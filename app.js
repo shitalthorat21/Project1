2f6ed437edb2339387102b9ac9456cce221dffab
shitalthorat21 <adviarvi2102@gmail.com>
2020-06-02 13:33:33 +0530

    second

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const mongoose=require('mongoose');
 const bodyparser=require('body-parser');
 const cors=require('cors');
 const path=require('path');
+
+const app=express();
+
+const route=require('./routes/route');
+
+//testing server
+
+const port=3000;
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', route);
+
+app.get('/',(req,res)=>{
+    res.send('Foobar');
+})
+app.listen(port,()=>{
+    console.log('Server started at port:'+port);
+});
+
+
